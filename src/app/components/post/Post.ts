e4a3a17491ee189757046809d45ff8a0a27c6d95
No <no@no.com>
2020-08-21 17:05:40 +0000

    Passing data to post component and displaying it

---
+++ src/app/components/post/Post.ts
@@ -0,0 +1,6 @@
+export class Post {
+    id: number;
+    authorId: number;
+    title: string;
+    body: string;
+}
--- src/app/components/wall/wall.component.ts
+++ src/app/components/wall/wall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post/Post';
 
 @Component({
   selector: 'app-wall',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wall.component.scss']
 })
 export class WallComponent implements OnInit {
+
+  posts: Post[] = [
+    {
+      id: 1,
+      authorId: 1,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      id: 2,
+      authorId: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    }
+  ]
 
   constructor() { }
 
